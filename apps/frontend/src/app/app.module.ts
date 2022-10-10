6ff092ab4291c019486d331294be26e764e09ee8
Jeffry Gonzalez <jeff@hypertheory.com>
2022-10-10 11:31:24 -0400

    settings

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  // note here
+}
