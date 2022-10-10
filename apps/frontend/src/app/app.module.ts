cc6bfed9b3d90b53360c28230de632de01d6303d
Jeffry Gonzalez <jeff@hypertheory.com>
2022-10-10 12:18:31 -0400

    added routing

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MastHeadComponent } from './components/mast-head/mast-head.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
     NxWelcomeComponent,
+    MastHeadComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
